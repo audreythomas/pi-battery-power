8e0de2a96fad29091da0112fda44b54893776bf2
Audrey Thomas <audrey.celeste.thomas@gmail.com>
2017-01-14 17:15:53 -0800

    Adding comments

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ from time import gmtime, strftime
 import sendgrid
 from sendgrid.helpers.mail import *
 
+# Write (append) messages to a file called battery_log.txt
 def write_log(logline):
   logfile = open('battery_log.txt', 'a')
   logfile.write(logline + '\n')
   logfile.close()
 
+# Send an email
+# NOTE: Be sure to change the from_email and to_email addresses
 def send_email(logline):
   sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-  from_email = Email("elmer@thinkingserious.com")
-  subject = "Science Project: Test 4"
-  to_email = Email("elmer.thomas+science2@gmail.com")
+  from_email = Email("test@example.com")
+  subject = "Science Project: The Raspberry Pi is Still Alive!"
+  to_email = Email("test@example.com")
   content = Content("text/plain", logline)
   mail = Mail(from_email, subject, to_email, content)
   try:
@@ -23,14 +26,21 @@ def send_email(logline):
   except:
     print "problem sending email"
 
-item = 'Battery log data'
+# Open a log file and write the title
+title = 'Battery Log Data'
 logfile = open('battery_log.txt', 'w')
-logfile.write(item + '\n')
+logfile.write(title + '\n')
 logfile.close()
 
+# Run the program until the Raspberry Pi runs out of battery power
 while True:
-  logline = str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-  print "adding %s to log file" % logline
-  write_log(logline)
-  send_email(logline)
+  log_time = str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+  message = "The Rasberry Pi is still alive at: %s" % log_time
+  # Print the message to the console screen
+  print(messsage)
+  # Write the message to our battery_log.txt file
+  write_log(message)
+  # Send an email with our message
+  send_email(message)
+  # Wait for 1 minute
   time.sleep(60)
